48d1ba6868b00791f0f09e1fdce7e1028be23056
MacBook Pro <aaa@bb.com>
2024-01-13 02:29:23 +0800

    update: 增加post

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1704993603';
+const cacheName = 'chirpy-1705084097';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
